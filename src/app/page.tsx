0cbb866963ac16a246afe145c8df25c24643f627
Haruki Nagasawa <hnagasawa57@gmail.com>
2025-02-11 20:05:50 +0900

    Use env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import ClientTop from "./ClientTop";
 
 async function getVipData() {
-  const response = await fetch("http://localhost:3000/api/kumamate", {
-    next: {
-      revalidate: 60 * 60, // 1 hour
-    },
-  });
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "/api/kumamate",
+    {
+      next: {
+        revalidate: 60 * 60, // 1 hour
+      },
+    }
+  );
   return response.json();
 }
 
