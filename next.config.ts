de53b64843e2f7df6f4b909696cf5590691001f6
Haruki Nagasawa <hnagasawa57@gmail.com>
2025-02-11 17:15:52 +0900

    Allow unsed vars

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  rules: {
+    "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
   /* config options here */
 };
 
